86b86bf159f930f84233725a624ed02aefcf7091
m1nd <m1nd@mail.ru>
2016-03-16 11:26:27 +0300

    Less02 Final

--- test.rb
+++ test.rb
@@ -46,103 +46,135 @@
 
 
 
-# class Point
+class Point
 
-# 	@@count = 0;
+  attr_accessor :x
+  attr_accessor :y
 
-# 	def initialize(x, y)
-# 		@x = x
-# 		@y = y
+	@@count = 0;
 
-# 		@@count += 1;
-# 	end
+	def initialize(x, y)
+		@x = x
+		@y = y
 
-# 	def x
-# 		@x
-# 	end
-
-# 	def y
-# 		@y
-# 	end	
-
-# 	def -(x1, y1)
-# 		x-x1 
-# 		y-y1
-# 	end
-
-# 	def self.printCount()
-# 		puts "count is #{@@count}"
-# 	end
-# end
-
-# pt = Point.new(4, 5)
-# pt2 = Point.new(2, 7)
-
-# pt.-(2,2)
-
-# p "#{pt.x} : #{pt.y}"
-
-
-
-
-class Lift
-  
-  attr_accessor :floor 	# На каком этаже лифт
-  attr_accessor :targetFoor
-
-  @@floorsCount = 10 	# Куда ехать
-
-  	# def initialize
-  	# 	self.floor = 0
-  	# end
-
-	def initialize(floor)
-		self.floor = floor
-		puts "Лифт готов"
+		@@count += 1;
 	end
 
-	def up
-		i = self.floor
-		until i > self.targetFoor do
-			Kernel.sleep(1)
-			puts "Едем вверх, этаж #{i}"
-			i += 1
-		end
-		puts "Приехали!"
+	def -(point)
+		@x = @x - point.x  
+		@y = @y - point.y
+		self
 	end
 
-	def down
-		i = self.floor
-		until i < self.targetFoor do
-			Kernel.sleep(1)		
-			puts "Едем вниз, этаж #{i}"
-			i -= 1
-		end
-		puts "Приехали!"
+	def +(point)
+		@x = @x + point.x  
+		@y = @y + point.y
+		self
 	end
 
 
-	def move(weight, target)
-		if weight < 400 && target <= @@floorsCount && target > 0
-			self.targetFoor = target
-			if target > self.floor
-					up()
-				else 
-					down()
-			end
-
-			else
-				puts "Не корректные данные!"
-		end
-		
+	def ==(point)
+		if @x == point.x && @y == point.y
+		 	true
+		 else
+		 	false
+		 end 
 	end
 
+	def isOrigin
+		if @x == 0 && @y == 0
+		 	true
+		 else
+		 	false
+		 end 
+	end
+
+	def setOrigin
+		@x = @y = 0
+	end
+
+	def self.printCount()
+		puts "count is #{@@count}"
+	end
 end
 
+pt = Point.new(4, 5)
+pt2 = Point.new(2, 7)
+pt3 = Point.new(4, 5)
+pt4 = Point.new(0, 0)
 
- liftOne = Lift.new(3)
- liftOne.move(350, 1)
 
- # liftTwo = Lift.new
- # puts liftTwo.floor
- # liftTwo.move(350, 6)
+resPoint = Point.new(3, 4)
+
+resPoint = pt - pt2
+puts "Result point = #{resPoint.x} : #{resPoint.y}"
+
+resPoint = resPoint + pt2
+puts "Result point = #{resPoint.x} : #{resPoint.y}"
+
+eq = pt3 == pt
+puts "Equal: #{eq}"
+
+puts "Is this origin? #{pt4.isOrigin}"
+
+puts "#{pt3.x} : #{pt3.y}"
+
+pt3.setOrigin
+
+puts "#{pt3.x} : #{pt3.y}"
+
+# class Lift
+  
+#   attr_accessor :floor 	# На каком этаже лифт
+#   attr_accessor :targetFoor
+
+#   @@floorsCount = 10 	# Куда ехать
+
+
+# 	def initialize(floor)
+# 		self.floor = floor
+# 		puts "Лифт готов"
+# 	end
+
+# 	def up
+# 		i = self.floor
+# 		until i > self.targetFoor do
+# 			Kernel.sleep(1)
+# 			puts "Едем вверх, этаж #{i}"
+# 			i += 1
+# 		end
+# 		puts "Приехали!"
+# 	end
+
+# 	def down
+# 		i = self.floor
+# 		until i < self.targetFoor do
+# 			Kernel.sleep(1)		
+# 			puts "Едем вниз, этаж #{i}"
+# 			i -= 1
+# 		end
+# 		puts "Приехали!"
+# 	end
+
+
+# 	def move(weight, target)
+# 		if weight < 400 && target <= @@floorsCount && target > 0
+# 			self.targetFoor = target
+# 			if target > self.floor
+# 					up()
+# 				else 
+# 					down()
+# 			end
+
+# 			else
+# 				puts "Не корректные данные!"
+# 		end
+		
+# 	end
+
+# end
+
+
+#  liftOne = Lift.new(6)
+#  liftOne.move(350, 4)
+
